b05b6c54c16c59406bfcc9286a71f61de4f88b79
Drew Crawford <drew@sealedabstract.com>
2024-08-21 18:56:55 -0500

    [aruntime] Spawn new runtime types

--- src/aruntime.rs
+++ src/aruntime.rs
@@ -1,10 +1,22 @@
+use std::fmt::Display;
 use std::future::Future;
 use aruntime::ARuntime;
 
 /**
 A runtime based on [spin_on]
 */
+#[derive(Debug, Copy, Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
 pub struct SpinRuntime;
+
+impl SpinRuntime {
+    pub const fn new() -> Self {
+        Self
+    }
+}
+
+
+
+
 impl ARuntime for SpinRuntime {
     fn spawn_detached<F: Future + Send>(&mut self, _priority: priority::Priority, _runtime_hint: aruntime::RuntimeHint, f: F) {
         crate::spin_on(f);
@@ -14,7 +26,13 @@ impl ARuntime for SpinRuntime {
 /**
 A runtime based on [sleep_on]
 */
+#[derive(Debug, Copy, Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
 pub struct SleepRuntime;
+impl SleepRuntime {
+    pub const fn new() -> Self {
+        Self
+    }
+}
 impl ARuntime for SleepRuntime {
     fn spawn_detached<F: Future + Send>(&mut self, _priority: priority::Priority, _runtime_hint: aruntime::RuntimeHint, f: F) {
         crate::sleep_on(f);
@@ -25,9 +43,64 @@ impl ARuntime for SleepRuntime {
 /**
 A runtime based on [spawn_on]
 */
+#[derive(Debug, Copy, Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
 pub struct SpawnRuntime;
+impl SpawnRuntime {
+    pub const fn new() -> Self {
+        Self
+    }
+}
 impl ARuntime for SpawnRuntime {
     fn spawn_detached<F: Future + Send + 'static>(&mut self, _priority: priority::Priority, _runtime_hint: aruntime::RuntimeHint, f: F) {
         crate::spawn_on(f);
     }
 }
+
+//boilerplate
+
+impl Display for SpinRuntime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SpinRuntime")
+    }
+}
+
+impl Display for SleepRuntime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SleepRuntime")
+    }
+}
+
+impl Display for SpawnRuntime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SpawnRuntime")
+    }
+}
+
+impl Default for SpinRuntime {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Default for SleepRuntime {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Default for SpawnRuntime {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)] mod test {
+    #[test] fn assert_send_sync() {
+        fn assert_send_sync<T: Send + Sync>() {}
+        assert_send_sync::<super::SpinRuntime>();
+        assert_send_sync::<super::SleepRuntime>();
+        assert_send_sync::<super::SpawnRuntime>();
+    }
+}
+
+
